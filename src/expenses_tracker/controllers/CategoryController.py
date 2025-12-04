6e0136c37a076963c43e227f49978cd19ec54377
texmo100 <isaac.ir@hotmail.com>
2025-12-03 17:21:57 -0700

    Fixed CategoryController to match its interface specifications

--- src/expenses_tracker/controllers/CategoryController.py
+++ src/expenses_tracker/controllers/CategoryController.py
@@ -3,18 +3,18 @@ from ..interfaces.ETControllerInterface import ETControllerInterface
 from ..utils.input_processors import string_input_processor, float_input_processor
 
 class CategoryController(ETControllerInterface):
-    def create(self):
+    def create(self, *args):
         new_category = Category()
         self._fill_info(new_category)
         print("New Category created sucessfully!")
         return new_category
     
-    def update(self, category):
-        self._fill_info(category)
+    def update(self, model, *args):
+        self._fill_info(model)
         print("Category updated sucessfully!")
 
-    def _fill_info(self, category):
-        category.name = string_input_processor("Enter the category name: ")
-        category.description = string_input_processor("Enter the category description: ")
-        category.limit = float_input_processor("Enter the category limit: ")
+    def _fill_info(self, model, *args):
+        model.name = string_input_processor("Enter the category name: ")
+        model.description = string_input_processor("Enter the category description: ")
+        model.limit = float_input_processor("Enter the category limit: ")
     
